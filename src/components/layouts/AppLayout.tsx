85027f412e97bb29d52e51eefc4f3b616880a093
saputt29 <sauki666indo@gmail.com>
2026-03-04 08:17:33 +0700

    feat : add folder pages/about and add folder layouts in components folder

---
+++ src/components/layouts/AppLayout.tsx
@@ -0,0 +1,18 @@
+import { type JSX } from "react";
+import Navbar from "../navbar";
+import Footer from "../Footer";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = (): JSX.Element => {
+  return (
+    <div className="h-svh">
+      <Navbar />
+      <div className="pt-24">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default AppLayout;
